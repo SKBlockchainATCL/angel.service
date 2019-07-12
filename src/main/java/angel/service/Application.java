3cb8daa9f9713386a96baa526e8e710266118748
Oh Sangmoon <halfface@chollian.net>
2019-07-12 18:17:26 +0900

    ...

--- src/main/java/angel/service/Application.java
+++ src/main/java/angel/service/Application.java
@@ -1,24 +1,43 @@
 package angel.service;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.annotation.concurrent.ThreadSafe;
+import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import angel.service.program.ServiceProgramController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+    ServiceProgramController.class
+})
 public class Application{
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
   public static void main(String[] args){
     // TODO Auto-generated method stub
-
+    SpringApplication.run(Application.class, args);
+  }
+  
+  
+  @Bean
+  public Docket restApi() {
+    
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .directModelSubstitute(LocalDate.class, String.class);
   }
 
 }
--- src/main/java/angel/service/program/ServiceProgramController.java
+++ src/main/java/angel/service/program/ServiceProgramController.java
@@ -9,28 +9,68 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import javax.annotation.concurrent.ThreadSafe;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Positive;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/servicePrograms",
+@RequestMapping(value = "/service/programs",
   produces = {MediaType.APPLICATION_JSON_UTF8_VALUE},
   consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
 @ParametersAreNonnullByDefault
 @ThreadSafe
 public class ServiceProgramController{
   
-  @PostMapping
-  public void addServiceProgram(String title, LocalDate startAt, LocalDate endAt, String detail, @Nullable String requestId) {
+  public static final int PAGE_SIZE_MAX = 20;
+  
+  public static final int PAGE_SIZE_DEFAULT = 5;
+  
+  public static final String PAGE_SIZE_DEFAULT_STRING = "5";
 
+  /**
+   * List or search service programs
+   * 
+   * @return
+   * @see GET service/programs
+   */
+  @GetMapping @Nonnull
+  public List<ServiceProgramValue> findServicePrograms(
+      @RequestParam("startAt") @Nullable String startAt,
+      @RequestParam("endAt") @Nullable String endAt,
+      @RequestParam("title") @Nullable String title,
+      @RequestParam(name = "pageSize", defaultValue = PAGE_SIZE_DEFAULT_STRING) 
+        @Positive @Max(PAGE_SIZE_MAX) int pageSize,
+      @RequestParam(name = "pageNo", defaultValue = "1") @Positive int pageNo
+  ){
+    
+    return new ArrayList<ServiceProgramValue>();
+  }
+  
+  @GetMapping("/open") @Nonnull
+  public List<ServiceProgramValue> findOpenServicePrograms(
+  ){
+    
+    return new ArrayList<ServiceProgramValue>();
+  }
+  
+  
+  @GetMapping("/started") @Nonnull
+  public List<ServiceProgramValue> findStartedServicePrograms(
+  ){
+    return new ArrayList<ServiceProgramValue>();
+  }
+  
+  @GetMapping("/completed") @Nonnull
+  public List<ServiceProgramValue> findCompletedServicePrograms(
+  ){
+    return new ArrayList<ServiceProgramValue>();
   }
 
-  
-  
-  
-  
 }
